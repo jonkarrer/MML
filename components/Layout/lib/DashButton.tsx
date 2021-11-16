8b1547ffa65d57a82f89df1ec1aab2e63d737a98
jonkarrer <karrerjon@gmail.com>
2021-11-16 10:02:25 -0600

    fix: first round of edits

--- components/Layout/lib/DashButton.tsx
+++ components/Layout/lib/DashButton.tsx
@@ -1,7 +1,9 @@
 const DashButton = () => (
   <a href="/dashboard">
     <span className="w-24 h-9 grid place-content-center bg-white rounded-full bg-opacity-20 hover:bg-opacity-40 cursor-pointer">
-      <h6 className="opacity-80 dark:text-white text-ty">Dashboard</h6>
+      <h6 className="opacity-80 dark:text-white text-ty font-bold">
+        Dashboard
+      </h6>
     </span>
   </a>
 );
