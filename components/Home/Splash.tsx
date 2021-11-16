78aaa070d6c5dfeb2b5feda5b8e310879e175d4b
jonkarrer <karrerjon@gmail.com>
2021-11-16 11:44:14 -0600

    feat: state effects

--- components/Home/Splash.tsx
+++ components/Home/Splash.tsx
@@ -5,7 +5,7 @@ const Splash: FC = () => {
   return (
     <section className="h-screen max-h-700 grid place-items-center bg-light_grad dark:bg-dark_grad">
       <article className="grid place-items-center text-center text-white">
-        <h1 className=" font-black md:w-130">Make the market you deserve</h1>
+        <h1 className="font-bold md:w-130">Make the market you deserve</h1>
         <p className="text-sm font-normal mt-12 md:w-120 md:text-base md:leading-10">
           Market alerts, statistics and analytics, delivered through an
           innovative interface, made for retail investors.
