f959a18b9b76e2c7b82b92397927408bbc29d43a
jonkarrer <karrerjon@gmail.com>
2021-11-18 13:33:33 -0600

    fix: color cards

--- components/Home/lib/Bar/Cards.tsx
+++ components/Home/lib/Bar/Cards.tsx
@@ -7,21 +7,21 @@ const Cards: FC = () => (
     <Card
       image="Dash"
       text="Use the Dashboard"
-      color="hover:bg-00DCCF"
+      color="bg-00DCCF lg:bg-white lg:dark:bg-121212 lg:hover:bg-00DCCF"
       link="/dashboard"
     />
 
     <Card
       image="Python"
       text="Start Developing"
-      color="hover:bg-01C2FF"
+      color="bg-01C2FF lg:bg-white lg:dark:bg-121212 lg:hover:bg-01C2FF"
       link="https://docs.marketmakerlite.com/"
     />
 
     <Card
       image="Discord"
       text="Join the community"
-      color="hover:bg-5F6CFF"
+      color="bg-5F6CFF lg:bg-white lg:dark:bg-121212 lg:hover:bg-5F6CFF"
       link="https://discord.gg/jjDcZcqXWy"
     />
   </div>
@@ -44,7 +44,7 @@ const Card = ({ image, text, color, link }: ICard) => {
       onClick={() => router.push(link)}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
-      className={`grid gap-9 place-content-center w-full py-7 rounded-xl lg:py-0 lg:gap-0 lg:h-full text-dark dark:text-lite lg:flex-1 cursor-pointer hover:shadow-card hover:text-white ${color}`}
+      className={`grid gap-9 place-content-center w-full py-7 rounded-xl lg:py-0 lg:gap-0 lg:h-full text-white lg:text-black dark:text-white lg:dark:text-lite lg:hover:text-white lg:flex-1 cursor-pointer hover:shadow-card ${color}`}
     >
       <div className="grid place-items-center gap-8 lg:h-36 lg:gap-0">
         {image === "Dash" ? (
