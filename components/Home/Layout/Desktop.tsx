d732d66d1f58d802b229536e82f5bebc01a94291
jonkarrer <karrerjon@gmail.com>
2021-12-15 13:41:47 -0600

    feat:support dropdown

--- components/Home/Layout/Desktop.tsx
+++ components/Home/Layout/Desktop.tsx
@@ -1,28 +1,58 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import NavLink from "./lib/Desktop/NavLink";
+import {
+  Start,
+  About,
+  Learn,
+  Blog,
+  Contact,
+  Triangle,
+} from "./lib/Support/index";
 
 const Desktop: FC = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <header className="hidden lg:block w-full absolute top-0 left-0 py-2">
-      <div className="m-auto w-179">
-        <nav className="grid grid-flow-col items-center gap-8 text-white">
-          <h6 className="text-xm font-black" style={{ marginBottom: "2px" }}>
-            mml
-          </h6>
+      <div className="wrapper flex items-center justify-between m-auto">
+        <h6 className="text-white text-xm font-black">mml</h6>
+        <nav className="grid grid-flow-col items-center gap-14 text-white">
           <NavLink text="Features" address="/#features" />
-          <NavLink text="Support" address="/support" />
+
+          <div className="text-white text-xm lg:text-14 font-medium cursor-pointer relative">
+            <p
+              onClick={() => setToggle(!toggle)}
+              className={`${toggle ? "opacity-60" : ""} hover:opacity-60`}
+            >
+              Support
+            </p>
+            <div
+              className={`${
+                toggle ? "absolute" : "hidden"
+              } bg-white dark:bg-121212 top-10 -left-8 w-59 h-60 p-6 flex flex-col justify-between rounded-base`}
+            >
+              <Start />
+              <About />
+              <Learn />
+              <Blog />
+              <Contact />
+              <span className="absolute top-0 left-12 transform -translate-y-3/4">
+                <Triangle />
+              </span>
+            </div>
+          </div>
+
           <NavLink
             text="Developers"
             address="https://docs.marketmakerlite.com/"
           />
           <NavLink text="Pricing" address="/pricing" />
           <NavLink text="Sign Up" address="/auth/signup" />
-          <a href="/dashboard">
-            <span className="w-24 h-9 grid place-content-center bg-white rounded-button bg-opacity-20 hover:bg-opacity-40 cursor-pointer">
-              <h6 className="dark:text-white text-14 font-medium">Dashboard</h6>
-            </span>
-          </a>
         </nav>
+        <a href="/dashboard">
+          <span className="w-24 h-9 grid place-content-center bg-white rounded-button bg-opacity-20 hover:bg-opacity-40 cursor-pointer">
+            <h6 className="text-white text-14 font-medium">Dashboard</h6>
+          </span>
+        </a>
       </div>
     </header>
   );
