d94f456ecc30c0f8598f20f25304bc8dc31401f1
jonkarrer <karrerjon@gmail.com>
2021-12-15 08:21:25 -0600

    Home page revisions, prepping support pages

--- components/Home/Bar.tsx
+++ components/Home/Bar.tsx
@@ -3,7 +3,7 @@ import Cards from "./lib/Bar/Cards";
 import Background from "./lib/Bar/Background";
 
 const Bar: FC = () => (
-  <section className="overflow-hidden">
+  <section className="overflow-hidden bg-F6F8FE dark:bg-121212 lg:bg-white lg:dark:bg-121212">
     <article className="wrapper grid gap-12 m-auto w-full lg:grid-cols-2 lg:gap-0 lg:h-60 relative">
       <div className="w-full flex items-center justify-center lg:justify-start">
         <Background />
