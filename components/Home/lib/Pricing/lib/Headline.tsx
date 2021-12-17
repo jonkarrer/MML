7bdcf65a9de55dc3450c559a9baee89719d8b6d7
jonkarrer <karrerjon@gmail.com>
2021-12-17 11:08:15 -0600

    change: card font color

--- components/Home/lib/Pricing/lib/Headline.tsx
+++ components/Home/lib/Pricing/lib/Headline.tsx
@@ -8,7 +8,7 @@ interface IHeadline {
 export const Headline: FC<IHeadline> = ({ title, desc, children }) => (
   <div className="grid gap-4 lg:gap-4 lg:pb-6 lg:border-b lg:border-D4D4D4">
     <h3 className="text-lg font-black">{title}</h3>
-    <p className=" text-232333 opacity-50 text-14 dark:text-lt.50">{desc}</p>
+    <p className="text-dk.50 text-14 dark:text-999999">{desc}</p>
     {children}
   </div>
 );
